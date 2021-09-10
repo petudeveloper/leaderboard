7ec22c72cdeae0990d4a37b0081c46514e19836b
petumazo <davidalvarezmazzo@gmail.com>
2021-09-09 23:26:31 -0500

    Add refresh and add functionality to a created game.

---
+++ src/dom-handler.js
@@ -0,0 +1,30 @@
+/* eslint-disable import/no-cycle */
+import { getScores, addScore } from './scores-handler';
+
+const form = document.getElementById('new-score-form');
+const refreshBtn = document.getElementById('refresh-item');
+const ul = document.getElementById('scores-list');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const userName = document.getElementById('user-name');
+  const userScore = document.getElementById('user-score');
+  addScore(userName.value, userScore.value);
+  userName.value = '';
+  userScore.value = '';
+});
+
+refreshBtn.addEventListener('click', () => {
+  ul.innerHTML = '';
+  getScores();
+});
+
+export default (scores) => {
+  scores.forEach((score) => {
+    const li = document.createElement('li');
+    li.innerHTML = `
+        <span>${score.user}: ${score.score} </span>    
+        `;
+    ul.appendChild(li);
+  });
+};
---
+++ src/scores-handler.js
@@ -0,0 +1,31 @@
+/* eslint-disable import/no-cycle */
+import renderScores from './dom-handler';
+
+const requestURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/LMXYZnWz9iXxoiT9U5dE/scores/';
+
+export const addScore = (name, score) => {
+  fetch(requestURL, {
+    method: 'POST',
+    body: JSON.stringify({
+      user: name,
+      score,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => json.result);
+};
+
+export const getScores = () => {
+  fetch(requestURL, {
+    method: 'GET',
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => json.result)
+    .then((data) => renderScores(data));
+};
